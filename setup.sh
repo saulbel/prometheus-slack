f58932bbe2bb303f58d5e4c6b73f7a4fb170984c
Ubuntu <saulitobel@gmail.com>
2022-08-01 02:42:22 +0200

    sync files mac/pc

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#users
+groupadd saul
+useradd -m saul -g saul
+#software installation
+apt update
+#nginx
+apt install nginx
+#docker
+apt install apt-transport-https ca-certificates curl gnupg lsb-release -y
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install docker-ce docker-ce-cli containerd.io -y
+#docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+#Change docker & docker-compose permissions
+usermod -aG docker saul
+chown docker:saul /usr/local/bin/docker-compose
